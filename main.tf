5e384d7f2ac43a5a6d8b9eb024bc0e1656cf69dc
Ubuntu <ubuntu@ip-172-31-43-143.eu-west-3.compute.internal>
2021-12-15 16:56:52 +0000

    Creating the repo for terraform course

---
+++ main.tf
@@ -0,0 +1,31 @@
+resource "null_resource" "dockervol" {
+  provisioner "local-exec" {
+    command = "mkdir noderedvol/ || true && sudo chown -R 1000:1000 noderedvol/"
+  }
+}
+
+module "image" {
+  source = "./image"
+  image_in = var.image[terraform.workspace]
+}
+
+resource "docker_container" "nodered_container" {
+  count = local.container_count
+  name  = join("-", ["nodered", terraform.workspace, random_string.random[count.index].result])
+  image = module.image.image_out
+  ports {
+    internal = 1880
+    external = var.ext_port[terraform.workspace][count.index]
+  }
+  volumes {
+    container_path = "/data"
+    host_path = "${path.cwd}/noderedvol"
+  }
+}
+
+resource "random_string" "random" {
+  count   = local.container_count
+  length  = 4
+  special = false
+  upper   = "false"
+}
---
+++ variables.tf
@@ -0,0 +1,34 @@
+
+variable "image" {
+  type = map
+  description = "image for container"
+  default = { 
+    dev = "nodered/node-red:latest"
+    prod = "nodered/node-red:latest-minimal"
+  }
+}
+
+
+variable "number_count" {
+  type    = number
+  default = 4
+}
+
+variable "ext_port" {
+  type    = map
+
+  validation {
+    condition     = max(var.ext_port["dev"]...) <= 65535 && min(var.ext_port["dev"]...) >= 1980
+    error_message = "The internal port must be within 0 and 65535."
+  }
+
+
+  validation {
+    condition     = max(var.ext_port["prod"]...) < 1980 && min(var.ext_port["prod"]...) >= 1880
+    error_message = "The internal port must be within 0 and 65535."
+  }
+}
+
+locals {
+  container_count = length(var.ext_port[terraform.workspace])
+}
