25caf75408ddbf815215ccd957eb792a383582a2
Ubuntu <ubuntu@ip-172-31-43-143.eu-west-3.compute.internal>
2021-12-30 17:07:10 +0000

    Module 3.57 of the course

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
-module "image" {
+module "nodered_image" {
   source   = "./image"
-  image_in = var.image[terraform.workspace]
+  image_in = var.image["nodered"][terraform.workspace]
+}
+
+module "influxdb_image" {
+  source   = "./image"
+  image_in = var.image["influxdb"][terraform.workspace]
 }
 
 module "container" {
@@ -8,12 +13,11 @@ module "container" {
   source            = "./container"
   count             = local.container_count
   name_in           = join("-", ["nodered", terraform.workspace, random_string.random[count.index].result])
-  image_in          = module.image.image_out
+  image_in          = module.nodered_image.image_out
   int_port_in       = 1880
-  ext_port_in       = var.ext_port[terraform.workspace][count.index]
+  ext_port_in       = var.ext_port[terraform.workspace][count.index]  
   container_path_in = "/data"
-  host_path_in      = "${path.cwd}/noderedvol"
-
+  
 }
 
 resource "random_string" "random" {
